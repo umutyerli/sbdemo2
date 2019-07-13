fc1f1ab426502dcca13dacbef255caac398ca7fc
Umut <umut@yerli.org>
2019-07-13 12:42:17 -0500

    init

---
+++ src/main/java/com/example/jdkdemo/JdkdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.jdkdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdkdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdkdemoApplication.class, args);
+	}
+
+}
